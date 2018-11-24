267ea9b6a24048725639a784bd05c02172edc5c7
Gijs Overvliet <gijs.overvliet@sogeti.com>
2018-11-24 14:38:07 +0100

    Renderloop

--- src/main/java/nl/ghyze/pitfall/Game.java
+++ src/main/java/nl/ghyze/pitfall/Game.java
@@ -8,13 +8,18 @@ import java.awt.*;
  */
 public class Game
 {
+    private boolean running = true;
+    private long startTime;
+
     public Game(){
         Window myWindow = new Window(null, null);
 
         try {
             ScreenFactory.getGraphicsDevice().setFullScreenWindow(myWindow);
             ScreenFactory.getGraphicsDevice().setDisplayMode(ScreenFactory.getDisplayMode());
-            Thread.sleep(10 * 1000);
+            startTime = System.currentTimeMillis();
+
+            renderLoop(myWindow.getGraphics());
 
         } catch (Exception e){
             e.printStackTrace();
@@ -25,11 +30,24 @@ public class Game
         System.exit(0);
     }
 
+    public void renderLoop(Graphics gr){
+        while(running){
+            gr.setColor(Color.blue);
+            gr.drawString("Hello world!", 512, 384);
+            checkTime();
+        }
+    }
+
+    private void checkTime(){
+        if (System.currentTimeMillis() - startTime > 10*1000){
+            running = false;
+        }
+    }
+
 
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         new Game();
     }
 }
